754c11470118804f544196affc5a20893ef2fd44
Nikita Tarasov <nikita@mygento.ru>
2016-04-19 23:05:52 +0300

    downgrade_php

--- manifests/fpm.pp
+++ manifests/fpm.pp
@@ -1,10 +1,27 @@
 # Class php::fpm
 class php::fpm (
-    $ensure = $php::ensure,
-    $pools  = $php::params::fpm_pools
+    $ensure  = $php::ensure,
+    $pools   = $php::params::fpm_pools,
+    $version = $php::version,
 ) inherits php::params {
 
     $real_package = "${php::params::package_prefix}${php::params::fpm_package}"
+
+    case $version {
+        '5.5': {
+            case $::lsbdistcodename {
+                'jessie': {
+                    apt::pin { 'downgrade_php_fpm':
+                      ensure   => 'present',
+                      packages => [$real_package],
+                      priority => 500,
+                      release  => 'wheezy-php55',
+                    }
+                }
+            }
+        }
+    }
+
 
     validate_string($ensure)
     validate_hash($pools)
