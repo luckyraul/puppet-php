d5d970d4e7d4e80702e943024841425d34745e4d
Nikita <luckyraul@users.noreply.github.com>
2016-12-22 01:37:45 +0300

    Update pecl.rb

--- lib/puppet/provider/package/pecl.rb
+++ lib/puppet/provider/package/pecl.rb
@@ -1,130 +1,113 @@
 require 'puppet/provider/package'
 
 Puppet::Type.type(:package).newparam(:pipe)
-Puppet::Type.type(:package).provide :pecl, :parent => Puppet::Provider::Package do
-  desc "PHP PEAR support. By default uses the installed channels, but you can specify the path to a pear package via ``source``."
+Puppet::Type.type(:package).provide :pecl, parent: Puppet::Provider::Package do
+  desc 'PHP pecl support. By default uses the installed channels, but you can specify the path to a pecl package via ``source``.'
 
   has_feature :versionable
   has_feature :upgradeable
 
   case Facter.value(:operatingsystem)
-    when "Solaris"
-      commands :pearcmd => "/opt/coolstack/php5/bin/pecl"
-    else
-      commands :pearcmd => "pecl"
+  when 'Solaris'
+    commands peclcmd: '/opt/coolstack/php5/bin/pecl'
+  else
+    commands peclcmd: 'pecl'
   end
 
-  def self.pearlist(hash)
-    command = [command(:pearcmd), "list"]
+  def self.pecllist(hash)
+    command = [command(:peclcmd), 'list']
 
     begin
-      list = execute(command).split("\n")
-      list = list.collect do |set|
-        if hash[:justme]
-          if /^#{hash[:justme]}/i.match(set)
-            if pearhash = pearsplit(set)
-              pearhash[:provider] = :pearcmd
-              pearhash
-            else
-              nil
-            end
-          else
-            nil
-          end
-        else
-          if pearhash = pearsplit(set)
-            pearhash[:provider] = :pearcmd
-            pearhash
-          else
-            nil
-          end
+      list = execute(command).split("\n").map do |set|
+        if hash[:justme] && %r{^#{hash[:justme]}$}i =~ set && (peclhash = peclsplit(set))
+          peclhash[:provider] = :peclcmd
+          peclhash
+        elsif (peclhash = peclsplit(set))
+          peclhash[:provider] = :peclcmd
+          peclhash
         end
-      end.reject { |p| p.nil? }
+      end.compact
     rescue Puppet::ExecutionFailure => detail
-      raise Puppet::Error, "Could not list pears: %s" % detail
+      raise Puppet::Error, format('Could not list pecls: %s', detail)
     end
 
-    if hash[:justme]
-      return list.shift
-    else
-      return list
-    end
+    return list.shift if hash[:justme]
+    list
   end
 
-  def self.pearsplit(desc)
+  def self.peclsplit(desc)
     desc.strip!
 
     case desc
-    when /^INSTALLED/ then return nil
-    when /No packages installed from channel/i then return nil
-    when /^=/ then return nil
-    when /^PACKAGE/ then return nil
-    when /^(\S+)\s+(\S+)\s+\S+/ then
-      name = $1
-      version = $2
+    when %r{^INSTALLED} then return nil
+    when %r{No packages installed from channel}i then return nil
+    when %r{^=} then return nil
+    when %r{^PACKAGE} then return nil
+    when %r{\[1m} then return nil # Newer versions of PEAR use colorized output
+    when %r{^(\S+)\s+(\S+)\s+\S+} then
+      name = Regexp.last_match(1)
+      version = Regexp.last_match(2)
 
       return {
-        :name => name,
-        :ensure => version
+        name: "pecl-#{name.downcase}",
+        ensure: version
       }
     else
-      Puppet.warning "Could not match %s" % desc
+      Puppet.warning format('Could not match %s', desc)
       nil
     end
   end
 
   def self.instances
-    pearlist(:local => true).collect do |hash|
+    pecllist(local: true).map do |hash|
       new(hash)
     end
   end
 
+  def peclname
+    name.sub('pecl-', '').downcase
+  end
+
   def install(useversion = true)
-    command = ["upgrade"]
+    command = ['upgrade']
 
-    if source = @resource[:source]
-      command << source
+    if @resource[:source]
+      command << @resource[:source]
+    elsif (!@resource.should(:ensure).is_a? Symbol) && useversion
+      command << '-f'
+      command << "#{peclname}-#{@resource.should(:ensure)}"
     else
-      if (! @resource.should(:ensure).is_a? Symbol) and useversion
-        command << "#{@resource[:name]}-#{@resource.should(:ensure)}"
-      else
-        command << "#{@resource[:name]}"
-      end
+      command << peclname
     end
 
-    if pipe = @resource[:pipe]
-        command << "<<<"
-        command << @resource[:pipe]
+    if @resource[:pipe]
+      command << '<<<'
+      command << @resource[:pipe]
     end
 
-    pearcmd(*command)
+    peclcmd(*command)
   end
 
   def latest
     version = ''
-    command = [command(:pearcmd), "remote-info", "#{@resource[:name]}"]
-      list = execute(command).lines.collect do |set|
-      if set =~ /^Latest/
-        version = set.split[1]
-      end
+    command = [command(:peclcmd), 'remote-info', peclname]
+    execute(command).each_line do |set|
+      version = set.split[1] if set =~ %r{^Latest}
     end
 
-    return version
+    version
   end
 
   def query
-    self.class.pearlist(:justme => @resource[:name])
+    self.class.pecllist(justme: peclname)
   end
 
   def uninstall
-    output = pearcmd "uninstall", "#{@resource[:name]}"
-    if output =~ /^uninstall ok/
-    else
-      raise Puppet::Error, output
-    end
+    output = peclcmd 'uninstall', peclname
+    raise Puppet::Error, output unless output =~ %r{^uninstall ok}
   end
 
   def update
-    self.install(false)
+    install(false)
   end
 end
