3489b93bf8e7352a9ad56e7db9792469194bc727
Nikita Tarasov <nikita@mygento.ru>
2017-02-27 00:11:18 +0300

    composer

--- manifests/composer.pp
+++ manifests/composer.pp
@@ -1,7 +1,18 @@
 # Class php::composer
 class php::composer (
+  $composer_source  = $php::params::composer_source,
+  $composer_path    = $php::params::composer_path,
+) {
 
-)
-{
+  ensure_packages(['curl'], {'ensure' => 'present'})
 
+  include ::staging
+
+  Package['curl'] -> staging::file { 'composer.phar':
+    source => $composer_source,
+  } -> file { $composer_path:
+      ensure  => 'present',
+      mode    => '0550',
+      source  => "${::staging::path}/php/composer.phar",
+  }
 }
