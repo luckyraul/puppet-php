cc9fefad703bad12697d8d61e7a1cf3a790243ac
nikita tarasov <nikita@mygento.ru>
2017-08-16 19:08:45 +0300

    stretch

--- manifests/globals.pp
+++ manifests/globals.pp
@@ -7,15 +7,11 @@ class php::globals (
   }
 
   $default_php_version = $::facts['os']['release']['major'] ? {
-        9 => '7.0',
-        default => '5.x',
+    '9' => '7.0',
+    default => '5.x',
   }
 
   $global_php_version = pick($php_version, $default_php_version)
-
-
-  # for debug output on the puppet client
-  notify {"PHP version: ${::global_php_version} with ${::operatingsystemrelease}":}
 
   Class['php::globals'] -> Class['php']
 }
