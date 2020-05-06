c6ca87f147c72f798cda2e9e2cfdb741356c548d
nikita tarasov <nikita@mygento.ru>
2020-05-06 13:24:44 +0300

    config require packages

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,17 +4,17 @@ class php::config(
   $version  = $php::version,
   )
 {
-  $defaults_cli = { 'path' => "${php::params::config_root}/cli/php.ini" }
+  $defaults_cli = { 'path' => "${php::params::config_root}/cli/php.ini", require => Class['php::packages'] }
   $result_cli = deep_merge($php::params::default_config, $settings)
   create_ini_settings($result_cli, $defaults_cli)
 
   if($php::fpm) {
-    $defaults_fpm = { 'path' => "${php::params::config_root}/fpm/php.ini" }
+    $defaults_fpm = { 'path' => "${php::params::config_root}/fpm/php.ini", require => Class['php::fpm'] }
     $result_fpm = deep_merge($php::params::default_config, $settings)
     create_ini_settings($result_fpm, $defaults_fpm)
   }
 
-  $defaults_opc = { 'path' => "${php::params::config_root}/mods-available/opcache.ini" }
+  $defaults_opc = { 'path' => "${php::params::config_root}/mods-available/opcache.ini", require => Class['php::packages']}
   $result_opc = deep_merge($php::params::opc_config, $settings)
   create_ini_settings($result_opc, $defaults_opc)
 }
